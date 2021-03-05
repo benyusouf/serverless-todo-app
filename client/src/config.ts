ee27ec04e9b8643f6e850f99aa4319486a030b72
Abdulllahi Yusuf <abdulllahiyusuf@ZEDCRESTs-MacBook-Pro-2.local>
2021-03-05 17:46:44 +0100

    staged config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,6 +5,6 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-west-2.amazonaws.com
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'benyusouf.eu.auth0.com',            // Auth0 domain
-  clientId: '3aRhWKh10Is79VtV1VQRYck4lJi8P3qL',          // Auth0 client id
+  clientId: 'HOWqYXmXerQdrGTFK2gml5zkMWt3ogoz',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
